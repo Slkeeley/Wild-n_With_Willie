3c5ba16b4d8a6dcdd5f10345644508837c22965e
Samuel Keeley <83038330+Slkeeley@users.noreply.github.com>
2023-03-09 17:40:01 -0800

    Screenshot taken

--- Assets/Scripts/TakePicture.cs
+++ Assets/Scripts/TakePicture.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class TakePicture : MonoBehaviour
 {
- public void captureImage()
+    public Image photo;
+    private Rect rectObj = new Rect(0,0, 100f, 200);
+    private Vector2 pivot = new Vector2(0, 0);
+
+    public void captureImage()
     {
-        Debug.Log("Captured"); 
-        ScreenCapture.CaptureScreenshot("Wild'n.png"); 
+        Debug.Log("Captured");
+        var texture = ScreenCapture.CaptureScreenshotAsTexture();
+        Sprite mySprite = Sprite.Create(texture,rectObj, pivot);
+        photo.sprite = mySprite;
     }
 }
