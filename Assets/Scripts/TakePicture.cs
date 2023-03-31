11ea2c50b7a55e0a5f5c77df7bca1cb979454e99
Samuel Keeley <83038330+Slkeeley@users.noreply.github.com>
2023-03-30 18:06:03 -0700

    Photo Save to Gallery

--- Assets/Scripts/TakePicture.cs
+++ Assets/Scripts/TakePicture.cs
@@ -3,20 +3,28 @@ using System.Collections.Generic;
 using System.IO; 
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Events; 
+using UnityEngine.Events;
 using TMPro;
 
 public class TakePicture : MonoBehaviour
 {
     public Image photo;
-    public UnityEvent screenSwitch;
+    public UnityEvent screenSwitch; 
     private Vector2 pivot = new Vector2(0, 0);
     Sprite mySprite;
    
 
-    public void captureImage()
+    public void takePhoto()
     {
-        screenSwitch?.Invoke(); 
+        screenSwitch?.Invoke();
+        StartCoroutine(captureImage());
+        
+    }
+
+
+    IEnumerator captureImage()//wait for a tenth of a second for the UI elements to disappear before taking the photo
+    {
+        yield return new WaitForSeconds(.1f);
         var texture = ScreenCapture.CaptureScreenshotAsTexture();
         if (mySprite == null)
         {
@@ -28,13 +36,13 @@ public class TakePicture : MonoBehaviour
             mySprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), pivot);
         }
         photo.sprite = mySprite;
-        saveSprite(); 
     }
+
 
     void saveSprite()
     {
         Texture2D photoSaved = mySprite.texture;
         byte[] bytes = photoSaved.EncodeToPNG();
-        File.WriteAllBytes(Application.persistentDataPath+"Wild'n", bytes); 
+        NativeGallery.SaveImageToGallery(bytes, "DCIM/album/filename", "Wild'n.jpg");
     }
 }
