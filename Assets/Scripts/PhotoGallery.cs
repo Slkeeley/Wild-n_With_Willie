d7effb4d6f2fb5a7b35cd3d16c8abd7240624677
Samuel Keeley <83038330+Slkeeley@users.noreply.github.com>
2023-04-19 20:47:21 -0700

    VIsual marker sprite

--- Assets/Scripts/PhotoGallery.cs
+++ Assets/Scripts/PhotoGallery.cs
@@ -7,6 +7,7 @@ using UnityEngine.Events;
 public class PhotoGallery : MonoBehaviour
 {
     [SerializeField] private Image currPhoto;
+    [SerializeField] private Image nextPhoto;
     private int photoIndex; 
     public Sprite[] photos;
     // Start is called before the first frame update
@@ -19,10 +20,16 @@ public class PhotoGallery : MonoBehaviour
 
     IEnumerator changePhoto()
     {
+        StartCoroutine(fadeOut()); 
         yield return new WaitForSeconds(5);
         photoIndex++;
         if (photoIndex >= photos.Length) photoIndex = 0; 
         currPhoto.sprite = photos[photoIndex]; 
         StartCoroutine(changePhoto());
+    }
+
+    IEnumerator fadeOut()
+    {
+        yield return new WaitForEndOfFrame(); 
     }
 }
