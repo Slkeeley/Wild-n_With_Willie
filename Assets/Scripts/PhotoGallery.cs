a3b329ac36145a11cfca4a05ed58ed4efad59b40
Samuel Keeley <83038330+Slkeeley@users.noreply.github.com>
2023-04-20 13:14:00 -0700

    Photo Fade In

--- Assets/Scripts/PhotoGallery.cs
+++ Assets/Scripts/PhotoGallery.cs
@@ -6,30 +6,87 @@ using UnityEngine.Events;
 
 public class PhotoGallery : MonoBehaviour
 {
-    [SerializeField] private Image currPhoto;
-    [SerializeField] private Image nextPhoto;
+    [SerializeField] private Image p1;
+    [SerializeField] private Image p2;
+    public Image currPhoto;
+    public Image nextPhoto;
     private int photoIndex; 
     public Sprite[] photos;
     // Start is called before the first frame update
     void Start()
     {
+        setPhotoOne(); 
         photoIndex = 0; 
-        currPhoto.sprite= photos[photoIndex]; 
-        StartCoroutine(changePhoto()); 
+        currPhoto.sprite= photos[photoIndex];
+        setNextPhoto();       
+    }
+
+
+    void setPhotoOne()
+    {
+        currPhoto = p1;
+        nextPhoto = p2;
+        currPhoto.color = new Color(1, 1, 1, 1);
+        nextPhoto.color = new Color(1, 1, 1, 0);
+    }
+
+    void setPhototwo()
+    {
+        currPhoto = p2;
+        nextPhoto = p1;
+        currPhoto.color = new Color(1, 1, 1, 1);
+        nextPhoto.color = new Color(1, 1, 1, 0);
+    }
+    void setNextPhoto()
+    {
+        photoIndex++;
+        if (photoIndex >= photos.Length) photoIndex = 0;
+        nextPhoto.sprite = photos[photoIndex];
+        StartCoroutine(changePhoto());
+    }
+    void setCurrPhoto()
+    {
+        if (currPhoto == p1)
+        {
+            Debug.Log("Setting Photo Two"); 
+            setPhototwo();
+            setNextPhoto(); 
+        }
+        else
+        {
+            Debug.Log("Setting Photo One");
+            setPhotoOne();
+            setNextPhoto(); 
+        }
     }
 
     IEnumerator changePhoto()
-    {
-        StartCoroutine(fadeOut()); 
-        yield return new WaitForSeconds(5);
-        photoIndex++;
-        if (photoIndex >= photos.Length) photoIndex = 0; 
-        currPhoto.sprite = photos[photoIndex]; 
-        StartCoroutine(changePhoto());
+    {   
+        yield return new WaitForSeconds(3);
+        StartCoroutine(fadeOut());
+        StartCoroutine(fadeIn()); 
     }
 
     IEnumerator fadeOut()
     {
-        yield return new WaitForEndOfFrame(); 
+        // loop over 1 second backwards
+        for (float i = 1.5f; i >= 0; i -= Time.deltaTime)
+        {
+            // set color with i as alpha
+            currPhoto.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+       
+    }
+
+    IEnumerator fadeIn()
+    {
+        for (float i = 0; i <= 1.5f; i += Time.deltaTime)
+        {
+            // set color with i as alpha
+            nextPhoto.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+        setCurrPhoto(); 
     }
 }
