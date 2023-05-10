f9737a02ee650a2dd47d57e4ea6fe43605ed9cd8
Samuel Keeley <83038330+Slkeeley@users.noreply.github.com>
2023-05-10 12:20:16 -0700

    Share Button Basics

--- Assets/Scripts/socialMediaButtons.cs
+++ Assets/Scripts/socialMediaButtons.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO; 
 
 public class socialMediaButtons : MonoBehaviour
 {
-
-    public void debugStatement()
+    [SerializeField] private TakePicture PhoneCamera;
+    public void clickShare()
     {
-        Debug.Log("pressed"); 
+        StartCoroutine(shareToSocials()); 
+    }
+
+    IEnumerator shareToSocials()
+    {
+        yield return new WaitForEndOfFrame();
+        if (PhoneCamera.mySprite != null)
+        {
+            Texture2D photoToShare = PhoneCamera.mySprite.texture;
+         
+            string filePath = Path.Combine(Application.temporaryCachePath, "shared img.png");
+            File.WriteAllBytes(filePath, photoToShare.EncodeToPNG());
+
+            Destroy(photoToShare);
+
+            new NativeShare().AddFile(filePath).SetSubject("Rockin' With Rue").SetText("#RockinWithRue").Share(); 
+        }
     }
 
 }
